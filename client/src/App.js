8f0beb8c582eb27ee5a240d7bc6ecc0279379227
ageuribeiro <ageu87@gmail.com>
2025-08-19 12:50:28 -0300

    Adicionando um componente para o Botão

--- client/src/App.js
+++ client/src/App.js
@@ -27,4 +27,21 @@ function App() {
   )
 }
 
-export default App
+function MyButton(){
+  return(
+    <button>I´m Button</button>
+  )
+}
+
+
+export default function MyApp(){
+  return (
+      <div>
+        <h1>
+          Bem-vindo ao meu aplicativo!
+        </h1>
+        <MyButton/>
+      </div>
+    );
+}
+
